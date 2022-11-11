522253b5aa28fef6d912c0d59947c6720d9bae1e
Home <ildar86@live.ru>
2022-11-11 14:20:53 +0500

    ДЗ1. Выполнено аание 2.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,29 @@ public class Main {
         //ДЗ-1.
 
         //Задание 1.
-        System.out.println("Циклы ч2. ДЗ-1. Задача 1");
-        int depositAmount = 15000;
-        int AmountOfSavings = 0;
-        int i = 0;
-        while ( AmountOfSavings <= 2_459_000 ) {
+        {
+            System.out.println("Циклы ч2. ДЗ-1. Задача 1");
+            int depositAmount = 15000;
+            int AmountOfSavings = 0;
+            int i = 0;
+            while (AmountOfSavings <= 2_459_000) {
+                i++;
+                AmountOfSavings = AmountOfSavings + AmountOfSavings / 100;
+                AmountOfSavings = AmountOfSavings + depositAmount;
+                System.out.println("Месяц " + i + " сумма накоплений равна " + AmountOfSavings + " рублей");
+            }
+        }
+        // Задание 2.
+        System.out.println("Циклы ч2. ДЗ-1. Задача 2");
+        {
+            int i = 0;
+        while (i < 10) {
             i++;
-            AmountOfSavings = AmountOfSavings + AmountOfSavings / 100;
-            AmountOfSavings = AmountOfSavings + depositAmount;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + AmountOfSavings + " рублей"); }
+            System.out.print(i + " ");}
+            System.out.println( );
+        }
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " "); }
 
 
     }
